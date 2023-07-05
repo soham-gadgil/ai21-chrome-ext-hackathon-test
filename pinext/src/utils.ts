78818d9e6cbd522e6ae512c493c1afb2488389da
cnm13ryan <channokman16@gmail.com>
2023-07-05 14:55:54 +0100

    fix: Added detect window color function

--- pinext/src/utils.ts
+++ pinext/src/utils.ts
@@ -1,12 +1,12 @@
-// import Browser from 'webextension-polyfill'
-// import { Theme } from './config'
+import Browser from 'webextension-polyfill'
+import { Theme } from './config'
 
-// export function detectSystemColorScheme() {
-//   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//     return Theme.Dark
-//   }
-//   return Theme.Light
-// }
+export function detectSystemColorScheme() {
+  if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    return Theme.Dark
+  }
+  return Theme.Light
+}
 
 export function getExtensionVersion() {
   return Browser.runtime.getManifest().version
