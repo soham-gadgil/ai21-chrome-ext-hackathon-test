0345097c83cef7262a81251d2fcf5377ce13b94e
cnm13ryan <channokman16@gmail.com>
2023-07-05 20:14:12 +0100

    feat: Changed API hosting URL
        Changed from localhost://8501 to https://pinext.streamlit.app/

--- pinext/src/api.ts
+++ pinext/src/api.ts
@@ -1,6 +1,6 @@
 import { getExtensionVersion } from './utils'
 
-const API_HOST = 'https://localhost:8501'
+const API_HOST = 'https://pinext.streamlit.app/'
 
 export interface PromotionResponse {
   url: string
