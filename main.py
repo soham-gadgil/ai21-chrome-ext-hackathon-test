74305c617c040e346cd214210e5cf1236793446e
soham-gadgil <sohamgadgil@googlemail.com>
2023-07-06 20:30:21 +0100

    Used PE in main.py to give extra info on results

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import re
 import os
 
 import ai21
 import streamlit as st
 from dotenv import load_dotenv
+from googlesearch import search
 
 # Load secrets
 load_dotenv()
@@ -11,19 +13,10 @@ API_KEY = os.getenv("AI21_LABS_API_KEY")
 
 ai21.api_key = API_KEY
 
-PROMPT = "Provide a list of the top 10 Chrome extensions that would be best suited to help users solve their problem.\nDescription of problem: {description}\n List: "
-
-# Initialization
 if "output" not in st.session_state:
-    st.session_state["output"] = "Output:"
+    st.session_state["output"] = ""
 
-
-def give_ext(inp):
-    if not len(inp):
-        return None
-
-    prompt = PROMPT.format(description=inp)
-
+def lmm_call(prompt):
     response = ai21.Completion.execute(
         model="j2-grande-instruct",
         prompt=prompt,
@@ -33,16 +26,59 @@ def give_ext(inp):
         numResults=10,
     )
 
-    st.session_state["output"] = response.completions[0].data.text
+    answer = response.completions[0].data.text
+    return answer
 
 
-st.title("Chrome Extension Recommender")
+def give_ext(inp):
 
-st.write(
-    "This is a simple **Streamlit** app that gives the name of the best Chrome Extension based on given description of a problem"
-)
+    if not len(inp):
+        return None
 
+    PROMPT_FORMAT = "Provide a list of the top 10 Chrome extensions that would be best suited to help users solve their problem.\nDescription of problem: {description}\n List: "
+    prompt = PROMPT_FORMAT.format(description=inp)
+
+    answer = lmm_call(prompt)
+    lines = answer.split('\n')
+
+    ext_names = []
+
+    # If LLM gives answer on 1 line
+    if len(lines) == 1:
+        # Split by number fullstop and space, then remove space from all except last entry
+        lines = re.split('\d+. ', lines[0])
+        for line in lines:
+            if len(line) > 1:
+                last_char = line[-1:]
+                if last_char == ' ':
+                    ext_names.append(line[:-1])
+                else:
+                    ext_names.append(line)
+    else:
+        # For each line LLM gives, split number from app name and and read in ext name
+        ext_names = []
+        for suggestion in [line.split('. ') for line in lines]:
+            if len(suggestion) == 2:
+                ext_names.append(suggestion[1])
+
+        ext_names = list(dict.fromkeys(ext_names))
+
+    results = []
+    for name in ext_names:
+        prompt = f"Describe the chrome extension that has the following name.\nName: {name}\n Answer: "
+        ext_desc = lmm_call(prompt)
+
+        ext_link = None
+        for answer in search(f'Chrome extension {name}', tld="com", num=10, stop=10, pause=1):
+            ext_link = answer
+            break
+
+        results.append([name, ext_desc, ext_link])
+
+    for result in results:
+        print(result)
+        print("--- --- ---")
 
 inp = st.text_area("Enter your description of your problem here", height=100)
-st.button("Suggest", on_click=give_ext(inp))
+st.button("Suggest", on_click=give_ext, args=[inp])
 st.write(f"Answer: {st.session_state.output}")
